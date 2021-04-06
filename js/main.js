c3617bbec34ba5766d1cb6295c7a66bed04b1074
chiefanunzio <chiefa.nunzio@gmail.com>
2021-04-06 16:13:38 +0200

    all stage

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function start(){
+
+
+    new Vue({
+
+    el:'#root',
+    data:{
+
+        'counter':'Quando mostro ai miei tutor che ho corretto un bug:',
+          image: "img/7.gif"
+    }
+    });
+}
+function init(){
+
+start();
+}
+$(init)
